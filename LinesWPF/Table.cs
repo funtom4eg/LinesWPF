66d4b3201663532a17e308924d7897e751adf20e
LENOVO <a.paladyuk@gmail.com>
2017-01-18 19:23:01 +0200

    1st model

---
+++ LinesWPF/Table.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace LinesWPF
+{
+    class Table
+    {
+        public int Size { get; set; }
+        public int Lines { get; set; }
+
+        Cell[,] cells;
+
+        public Table(int size = 5)
+        {
+            this.Size = size;
+            
+        }
+
+    }
+
+    abstract class Cell
+    {
+
+        int X { get; set; }
+        int Y { get; set; }
+
+    }
+
+    class EmptyCell : Cell
+    {
+
+    }
+
+    class EndPointCell : Cell
+    {
+
+    }
+
+    class LineCell : Cell
+    {
+
+    }
+
+}
