656ae044c0fbbf4b096f05deec7ff4cfefa5cd48
sony <sony@VAIO>
2017-01-19 14:37:18 +0200

    last ver vaio

---
+++ LinesConsole/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinesConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Table table = new Table(6);
+            Console.WriteLine(table.cells[1, 1].Left.GetType().Name);
+            Console.WriteLine(table.cells[1,1].Right.Equals(table.cells[3,1].Left));
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ LinesWPF/Classes/Table.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace LinesWPF
+{
+    enum Directions { tb, lr, tr, tl, br, bl}; //tb - top-bottom, lr - left-right, tr - top-right etc.
+    class Table
+    {
+        public int Size { get; set; }
+        public int Colors { get; set; }
+
+        Cell[,] cells;
+
+        public Table(int size)
+        {
+            //setting x,y size
+            if (size > 5)
+                this.Size = size;
+            else this.Size = 5;
+
+            cells = new EmptyCell[size, size];
+
+            //Initializing (0,0) cell
+            cells[0, 0] = new EmptyCell();
+
+            //Filling table with empty cells
+            for (int j = 0; j < size; j++)
+                for (int i = 0; i < size; i++)
+                {
+                    //Initialization flow:
+                    //------------>
+                    //------------>
+                    //------------>
+                    //------------>
+                    //------------>
+
+                    //1. Every cell with y>0 is initialized with bottom field of upper cell
+                    //2. In first row every cell with x>0 is initialized with right field of left cell
+                    //3. Otherways cells[0,0] is initialized with new EmptyCell();
+                    if (j != 0)
+                        cells[i, j] = cells[i, j - 1].Bottom;
+                    else if (i > 0)
+                        cells[i, j] = cells[i - 1, j].Right;
+                    else cells[i, j] = new EmptyCell();
+
+                    //Assigning left cells - null in the left column
+                    cells[i, j].Left = (i - 1) < 0 ? null : cells[i - 1, j];
+
+                    //Assigning top cells - null in the first row
+                    cells[i, j].Top = (j - 1) < 0 ? null : cells[i, j - 1];
+
+                    //Assigning right cells - null in the right column. if j>0 it corresponds with bottom field of upper-right. in 1st row - new();
+                    cells[i, j].Right = (i + 1) >= size ? null : (j > 0) ? cells[i + 1, j - 1].Bottom : new EmptyCell();
+
+                    //Assigning bottom cells - null in the last row and new() for other cells
+                    cells[i, j].Bottom = (j + 1) >= size ? null : new EmptyCell();
+                }
+
+
+        }
+
+    }
+
+    abstract class Cell
+    {
+        //Neigbour cells, null = out of table
+        public Cell Top { get; set; }
+        public Cell Bottom { get; set; }
+        public Cell Left { get; set; }
+        public Cell Right { get; set; }
+
+    }
+
+    class EmptyCell : Cell
+    {
+        public EmptyCell()
+        {
+        }
+
+    }
+
+    class EndPointCell : Cell
+    {
+        Color color;
+
+    }
+
+    class LineCell : Cell
+    {
+        Color color;
+        Directions direction;
+
+    }
+
+}
